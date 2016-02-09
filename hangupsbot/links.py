bb05c0e1509886db3fe59722456992cec51f1dbc
Omkar Kulkarni <2019okulkarn@tjhsst.edu>
2016-02-09 09:34:30 -0500

    new get_title method

--- hangupsbot/links.py
+++ hangupsbot/links.py
@@ -1,28 +1,47 @@
-from bs4 import BeautifulSoup as BS
-from urllib.request import urlopen
-from requests import get
 import json
 from urllib.parse import quote
+import re
+from lxml.html import document_fromstring
+from requests import exceptions, get, post
 
 def shorten(url):
     try:
-        response = urlopen('http://tinyurl.com/api-create.php?url=' + url)
-        html = response.read()
-        soup = BS(html, 'html.parser')
-        return soup.get_text()
+        response = get('http://tinyurl.com/api-create.php?url=' + url)
+        return str(response.text)
     except:
         error = "Could not create shortened link"
         return error
 
+#Code for get_title is borrowed from http://github.com/tjcsl/cslbot/
 def get_title(url):
-    try:
-        response = urlopen(url)
-        html = response.read()
-        soup = BS(html, 'html.parser')
-        return soup.title.string
-    except:
-        error = "Title not Found"
-        return error
+    title = 'No Title Found'
+    # User-Agent is really hard to get right :(
+    headers = {'User-Agent': 'Mozilla/5.0 HangoutsBot'}
+    req = get(url, headers=headers)
+    ctype = req.headers.get('Content-Type')
+    if req.status_code != 200:
+        title = 'HTTP Error %d: %s' % (req.status_code, req.reason)
+    elif ctype is not None and ctype.startswith('image/'):
+        title = 'Image'
+    else:
+        html = document_fromstring(req.content)
+        t = html.find('.//title')
+        # FIXME: is there a cleaner way to do this?
+        if t is not None and t.text is not None:
+        # Try to handle multiple types of unicode.
+            try:
+                title = bytes(map(ord, t.text)).decode('utf-8')
+            except (UnicodeDecodeError, ValueError):
+                title = t.text
+                title = ' '.join(title.splitlines()).strip()
+        # If we have no <title> element, but we have a Content-Type, fall back to that
+        elif ctype is not None:
+            title = ctype
+        else:
+            title = "Title Not Found"
+    return title
+
+
 
 def lengthen(url):
     tolengthen = quote(url)
