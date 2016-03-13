934fcc2e8e2c02f7e11c82c1719b7b607d96130a
Omkar Kulkarni <2019okulkarn@tjhsst.edu>
2016-03-12 23:02:00 -0500

    update vars and add help

--- hangupsbot/plugins/memo.py
+++ hangupsbot/plugins/memo.py
@@ -43,11 +43,10 @@ def add_memo(bot, event, name, text):
 
 
 def memo(bot, event, *args):
-    id_ = add_memo(bot, event, args[0], ' '.join(args[1:]))
-    if id_:
-        yield from bot.coro_send_message(event.conv, _(id_))
-    else:
-        yield from bot.coro_send_message(event.conv, _("No user by that name"))
+    '''Leaves a memo for someone. Format is /bot memo <name> <message>'''
+    added = add_memo(bot, event, args[0], ' '.join(args[1:]))
+    if added:
+        yield from bot.coro_send_message(event.conv, _(added)
 
 
 @asyncio.coroutine
