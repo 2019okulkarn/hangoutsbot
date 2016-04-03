980d4defd614186b1abe4f2c4bf12bbce90cb297
Omkar Kulkarni <2019okulkarn@tjhsst.edu>
2016-04-03 13:50:27 -0400

    just send day and admin can use topic command

--- hangupsbot/plugins/today.py
+++ hangupsbot/plugins/today.py
@@ -1,5 +1,4 @@
 import plugins
-from commands import command
 from bs4 import BeautifulSoup as Soup
 from requests import get
 
@@ -9,6 +8,5 @@ def _initialize():
 def today(bot, event, *args):
     r = get("http://www.nationaldaycalendar.com/feed/")
     soup = Soup(r.text, "lxml")
-    day = soup.item.title.string
-    cmd = ["topic", "{} | http://i.okulkarni.cf/Rules.jpg".format(day)]
-    yield from command.run(bot, event, *cmd)
+    day = soup.item.title.string.title()
+    yield from bot.coro_send_message(event.conv, _(day)) 
