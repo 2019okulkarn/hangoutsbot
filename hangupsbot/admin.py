fc55f1a2ee5f91d779186b6601fee4fe3ae26a91
Omkar Kulkarni <2019okulkarn@tjhsst.edu>
2016-04-05 22:37:29 -0400

    more admin functions

--- hangupsbot/admin.py
+++ hangupsbot/admin.py
@@ -1,6 +1,17 @@
-def is_admin(bot, event):
+def is_admin(bot, event, other=None):
     admins_list = bot.get_config_suboption(event.conv_id, 'admins')
     if event.user_id.chat_id in admins_list:
         return True
+    else:
+        return False
+
+
+def is_tag(bot, event, tag=None):
+    tags = bot.user_memory_get(event.user.id_.chat_id, 'tags')
+    if tags:
+        if tag in tags:
+            return True
+        else:
+            return False
     else:
         return False
