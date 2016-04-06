90a8f5a0431021ab4a6240a7507757aa46cc6312
Srijay Kasturi <srijay@techfilmer.com>
2016-04-06 09:11:07 -0400

    yay quote approval

--- hangupsbot/plugins/quote.py
+++ hangupsbot/plugins/quote.py
@@ -4,21 +4,27 @@ from control import *
 from admin import is_admin, is_tag
 from ixio import ixio
 
-
 def _initialise():
-   plugins.register_user_command('quote')
-   conn = sqlite3.connect('bot.db')
-   c = conn.cursor()
-   c.execute("CREATE TABLE IF NOT EXISTS quotes (author TEXT, quote TEXT, id INTEGER PRIMARY KEY AUTOINCREMENT)")
-   conn.commit()
-   conn.close()
+    plugins.register_user_command('quote')
+    conn = sqlite3.connect('bot.db')
+    c = conn.cursor()
+    c.execute("CREATE TABLE IF NOT EXISTS quotes (author TEXT, quote TEXT, id INTEGER PRIMARY KEY AUTOINCREMENT)")
+    c.execute("CREATE TABLE IF NOT EXISTS unapp_quotes (author TEXT, quote TEXT, id INTEGER PRIMARY KEY AUTOINCREMENT)")   
+    conn.commit()
+    conn.close()
 
-def add(conn, quote, author):
-   c = conn.cursor()
-   c.execute("INSERT INTO quotes(author, quote) VALUES (?, ?)", [author.lower(), quote])
-   conn.commit()
-   rply = "Successfully added quote!"
-   return rply
+def add(conn, quote, author, admin=True):
+    c = conn.cursor()
+    if admin:
+        c.execute("INSERT INTO quotes(author, quote) VALUES (?, ?)", [author.lower(), quote])
+        conn.commit()
+        rply = "Successfully added quote!"
+    else:
+        c.execute("INSERT INTO unapp_quotes(author, quote) VALUES (?, ?)", [author.lower(), quote])
+        conn.commit()
+        rply = ["Quote submitted for approval.",
+            "New quote submitted for approval. To approve, do\n! approve quote {}".format(c.lastrowid)]
+    return rply
 
 def retrieve(conn, id_, author, full=True):
     c = conn.cursor()
@@ -28,7 +34,6 @@ def retrieve(conn, id_, author, full=True):
             q = c.fetchone()
             rply = format_quote(q)
         else:
-            print("wassup ya boi is here")
             c.execute('SELECT * FROM quotes')
             q = c.fetchall()
             quotes = []
@@ -74,24 +79,18 @@ def format_quote(q):
     return quote
 
 def quote(bot, event, *args):
-   msg = None
-   try:
+    msg = None
+    try:
         conn = sqlite3.connect('bot.db')
         if not args:
             msg = retrieve(conn, None, False)
         elif args[0] not in ['-a', '-d', '-l', '-e'] and args[0].startswith('-'):
             msg = "Invalid Flag"
-        elif args[0] in ['-a', '-d', '-e']:
+        elif args[0] in ['-d', '-e']:
             if len(args) < 2:
                msg = "You're missing arguments!"
             elif is_admin(bot, event) or is_tag(bot, event, 'quote-admin'): # admin only quote functions
-                if args[0] == "-a": # edit quotes
-                    text = " ".join(args[1:]).split(' - ')
-                    if event.user.first_name.lower() == text[1]:
-                        msg = "You can't submit your own quote!" # self-submission
-                    else:
-                        msg = add(conn, text[0], text[1])
-                elif args[0] == "-d": # delete quotes
+                if args[0] == "-d": # delete quotes
                     delete(conn, args[1])
                     msg = "Successfully deleted quote"
                 elif args[0] == "-e": # edit quotes
@@ -100,10 +99,20 @@ def quote(bot, event, *args):
                 msg = "You're not an admin!"
         elif args[0] == "-l": # list quotes from author
             if len(args) == 1:
-                print("hi i made it here")
                 msg = str(retrieve(conn, None, True))
             else:
                 msg = str(retrieve(conn, args[1], True))
+        elif args[0] == "-a":
+            text = " ".join(args[1:]).split(' - ')
+            if event.user.first_name.lower() == text[1]:
+                msg = "You can't submit your own quote!" # self-submission
+            else:
+                if is_admin(bot, event) or is_tag(bot, event, 'quote-admin'):
+                    msg = add(conn, text[0], text[1])
+                else:
+                    rply = add(conn, text[0], text[1], admin=False)
+                    yield from bot.coro_send_message(CONTROL, _(rply[1]))
+                    msg = rply[0]
         else:
             text = " ".join(args)
             author = True if not text.isnumeric() else False
@@ -112,15 +121,15 @@ def quote(bot, event, *args):
         print(msg)
         split = msg.split('\n')
         if len(split) > 4:
-            msg = "<b>Message truncated:</b> See full message at {}\n Here are the last 4 lines: \n{}".format(
+            msg = "<b>Message truncated:</b> See full message at {}\nHere are the last 4 lines: \n{}".format(
             	ixio(msg), "\n".join(split[-4:])) 
         #yield from bot.coro_send_message(event.conv, _(str(len(leng))))        
         yield from bot.coro_send_message(event.conv, _(msg))
         conn.close()
-   except TypeError:
+    except TypeError:
         msg = 'No such quote'
         yield from bot.coro_send_message(event.conv, _(msg))
-   except BaseException as e:
+    except BaseException as e:
         msg = '{} -- {}'.format(str(e), event.text)
         raise e
         yield from bot.coro_send_message(CONTROL, _(msg))
