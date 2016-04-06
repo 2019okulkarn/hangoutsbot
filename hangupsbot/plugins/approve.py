f938ee29ab9df530221b70d1bba2c45f01c4b819
Srijay Kasturi <srijay@techfilmer.com>
2016-04-06 09:11:07 -0400

    yay quote approval

--- hangupsbot/plugins/approve.py
+++ hangupsbot/plugins/approve.py
@@ -1,12 +1,12 @@
 import plugins
 from commands import command
-
+import sqlite3
 
 def _initialize():
     plugins.register_admin_command(['approve'])
 
-
 def approve(bot, event, *args):
+    conn = sqlite3.connect('bot.db')
     request = args[0]
     request_number = str(args[1])
     if request.lower() == 'poll':
@@ -21,6 +21,12 @@ def approve(bot, event, *args):
             yield from command.run(bot, event, *command_to_run)
             yield from bot.coro_send_message(conversation_id, _("Poll {} approved").format(request_number))
             return
-        return
+    elif request.lower() == 'quote':
+        c = conn.cursor()
+        c.execute('SELECT * FROM unapp_quotes WHERE id = ?', [request_number])
+        q = c.fetchone() 
+        c.execute("INSERT INTO quotes(author, quote) VALUES (?, ?)", [q[0], q[1]])
+        conn.commit()
+        yield from bot.coro_send_message(event.conv, _("Quote {} approved").format(c.lastrowid))
     else:
         yield from bot.coro_send_message(event.conv, _("Approval for that not yet supported"))
