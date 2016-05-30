ab6dc2ad5697cd58b788b9f263a28c01fbb3736c
Omkar Kulkarni <2019okulkarn@tjhsst.edu>
2016-05-30 19:20:36 -0400

    spacing

--- hangupsbot/plugins/stringops.py
+++ hangupsbot/plugins/stringops.py
@@ -33,7 +33,7 @@ def convert(strlist):
         for char in string:
             toappend += chr(0xFEE0 + ord(char))
         toreturn.append(toappend)
-    return ' '.join(toreturn)
+    return '  '.join(toreturn)
 
 def fullwidth(bot, event, *args):
     if args:
