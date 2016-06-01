a5c9c0228c661eda0829ffc2e675a5cb8556f64a
Omkar Kulkarni <2019okulkarn@tjhsst.edu>
2016-05-31 23:32:28 -0400

    idk why this fixes things

--- hangupsbot/plugins/meme.py
+++ hangupsbot/plugins/meme.py
@@ -14,17 +14,13 @@ def _initialize():
     plugins.register_user_command(["meme"])
 
 def meme(bot, event, *args):
-    try:
-        client = ImgurClient(imgur_client, imgur_secret)
-        funny = client.subreddit_gallery('funny')
-        link_image = random.choice(funny).link
-        logger.info("getting {}".format(link_image))
-        filename = os.path.basename(link_image)
-        r = yield from aiohttp.request('get', link_image)
-        raw = yield from r.read()
-        image_data = io.BytesIO(raw)
-        image_id = yield from bot._client.upload_image(image_data, filename=filename)
-        yield from bot.coro_send_message(event.conv.id_, None, image_id=image_id)
-    except Exception e:
-        msg = _('{} -- {}').format(str(e), event.text)
-        yield from bot.coro_send_message(CONTROL, msg)
+    client = ImgurClient(imgur_client, imgur_secret)
+    funny = client.subreddit_gallery('funny')
+    link_image = random.choice(funny).link
+    logger.info("getting {}".format(link_image))
+    filename = os.path.basename(link_image)
+    r = yield from aiohttp.request('get', link_image)
+    raw = yield from r.read()
+    image_data = io.BytesIO(raw)
+    image_id = yield from bot._client.upload_image(image_data, filename=filename)
+    yield from bot.coro_send_message(event.conv.id_, None, image_id=image_id)
