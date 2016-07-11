3bb955129e8f2f6629a74caa2ec8eab46f932456
Srijay Kasturi (TechFilmer) <srijay@techfilmer.com>
2016-07-10 20:42:16 -0400

    aww cmd

--- hangupsbot/plugins/meme.py
+++ hangupsbot/plugins/meme.py
@@ -11,7 +11,7 @@ import io
 logger = logging.getLogger(__name__)
 
 def _initialize():
-    plugins.register_user_command(["meme"])
+    plugins.register_user_command(["meme", "aww"])
 
 def meme(bot, event, *args):
     try:
@@ -28,3 +28,20 @@ def meme(bot, event, *args):
     except Exception as e:
         msg = _('{} -- {}').format(str(e), event.text)
         yield from bot.coro_send_message(CONTROL, msg)
+        
+def aww(bot, event, *args):
+    try:
+        client = ImgurClient(imgur_client, imgur_secret)
+        funny = client.subreddit_gallery('aww')
+        link_image = random.choice(funny).link
+        logger.info("getting {}".format(link_image))
+        filename = os.path.basename(link_image)
+        r = yield from aiohttp.request('get', link_image)
+        raw = yield from r.read()
+        image_data = io.BytesIO(raw)
+        image_id = yield from bot._client.upload_image(image_data, filename=filename)
+        yield from bot.coro_send_message(event.conv.id_, None, image_id=image_id)
+    except Exception as e:
+        msg = _('{} -- {}').format(str(e), event.text)
+        yield from bot.coro_send_message(CONTROL, msg)
+
